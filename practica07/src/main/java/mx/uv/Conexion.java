5d7213a181956acc89d96062a284dd7e86c22aef
Jorfuje <112489240+Jorfuje@users.noreply.github.com>
2022-11-10 13:20:32 -0600

    Iniciando BD

---
+++ practica07/src/main/java/mx/uv/Conexion.java
@@ -0,0 +1,27 @@
+package mx.uv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static String url = "jdbc:mysql//127.0.0.1:3306/nombreBD";
+    private static String DriverName = "com.mysql.jdbc.Driver";
+    private static String username = "root";
+    private static String password = "";
+    private static Connection connection = null;
+
+
+    public static Connection getConnection(){
+        try{
+            Class.forName(DriverName);
+            connection = DriverManager.getConnection(url, username, password);
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        } catch (ClassNotFoundException e){
+            System.out.println("no se encontro el driver");
+        }
+        return connection;
+    }
+}
+
